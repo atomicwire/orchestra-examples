c2fcb6621fbf7f90ee810f89c1cdc75e2d2d966e
Kevin Race <kevin@atomicwire.io>
2024-09-13 15:27:56 +0100

    [AWP-1340] Add example QuickFIX app

---
+++ app-examples/02-quickfix/src/main/java/io/atomicwire/quickfix/QuickFixClientDataDictionaryApp.java
@@ -0,0 +1,121 @@
+/*
+ * Copyright 2024 Atomic Wire Technology Limited
+ * SPDX-License-Identifier: Apache-2.0
+ */
+package io.atomicwire.quickfix;
+
+import static io.atomicwire.quickfix.QuickFixEngineDataDictionaryApp.checkArgs;
+import static io.atomicwire.quickfix.QuickFixEngineDataDictionaryApp.prettyLog;
+
+import java.time.LocalDateTime;
+import lombok.extern.slf4j.Slf4j;
+import quickfix.*;
+import quickfix.field.*;
+
+@Slf4j
+public class QuickFixClientDataDictionaryApp implements Application {
+
+  @Override
+  public void onCreate(SessionID sessionId) {
+    log.info("Client session created: {}", prettyLog(sessionId));
+  }
+
+  @Override
+  public void onLogon(SessionID sessionId) {
+    log.info("Client logged on: {}", prettyLog(sessionId));
+  }
+
+  @Override
+  public void onLogout(SessionID sessionId) {
+    log.info("Client logged out: {}", prettyLog(sessionId));
+  }
+
+  @Override
+  public void toAdmin(Message message, SessionID sessionId) {
+    log.info("Client sending admin message: {}", prettyLog(message));
+  }
+
+  @Override
+  public void fromAdmin(Message message, SessionID sessionId) {
+    log.info("Client received admin message: {}", prettyLog(message));
+  }
+
+  @Override
+  public void toApp(Message message, SessionID sessionId) {
+    log.info("Client sending application message: {}", prettyLog(message));
+  }
+
+  @Override
+  public void fromApp(Message message, SessionID sessionId) {
+    log.info("Client received application message: {}", prettyLog(message));
+  }
+
+  public void sendValidRequest(SessionID sessionID) throws SessionNotFound {
+    var validRequest = getBaseMessage(true);
+    Session.sendToTarget(validRequest, sessionID);
+  }
+
+  /** Method to send messages that don't conform to the data dictionary */
+  public void sendInvalidRequests(SessionID sessionID) throws SessionNotFound {
+    var missingRequiredFieldMessage = getBaseMessage(false);
+    Session.sendToTarget(missingRequiredFieldMessage, sessionID);
+
+    var invalidFieldMessage = getBaseMessage(true);
+    invalidFieldMessage.setField(new SecondaryOrderID("ZYX987"));
+    Session.sendToTarget(invalidFieldMessage, sessionID);
+  }
+
+  private Message getBaseMessage(Boolean includeAccountField) {
+    var message = new Message();
+
+    message.getHeader().setField(new MsgType(MsgType.ORDER_SINGLE));
+
+    if (includeAccountField) {
+      message.setField(new Account("ACC123"));
+    }
+    message.setField(new ClOrdID("ORD12123"));
+    message.setField(new Side(Side.SELL));
+    message.setField(new TransactTime(LocalDateTime.now()));
+    message.setField(new OrdType(OrdType.MARKET));
+    message.setField(new SecurityID("AAPL"));
+    message.setField(new OrderQty(3500));
+
+    return message;
+  }
+
+  public static void main(String[] args) throws ConfigError, SessionNotFound, InterruptedException {
+    checkArgs(args);
+
+    // Load the client session settings from a configuration file
+    var settings = new SessionSettings("quickfix-client.cfg");
+    // the Data Dictionary is passed in - it's the path to the output of the
+    // basic-examples/08-quickfix sub-project
+    settings.setString("AppDataDictionary", args[0]);
+
+    // Create the FIX client application instance
+    var clientApp = new QuickFixClientDataDictionaryApp();
+    // Create a message store factory (memory-based in this example)
+    var storeFactory = new MemoryStoreFactory();
+
+    // Initialize the client initiator with all components
+    var initiator =
+        new SocketInitiator(clientApp, storeFactory, settings, null, new DefaultMessageFactory());
+    initiator.start();
+
+    // Allow some time to establish a connection
+    Thread.sleep(2000);
+
+    // This example application only has a single session configured
+    var sessionID = initiator.getSessions().get(0);
+
+    log.info("Sending valid request");
+    clientApp.sendValidRequest(sessionID);
+
+    log.info("Sending invalid requests");
+    clientApp.sendInvalidRequests(sessionID);
+
+    // Sleep to allow responses to be received
+    Thread.sleep(1000);
+    initiator.stop();
+  }
+}
--- basic-examples/08-quickfix/orchestra/specification/08-quickfix.md
+++ basic-examples/08-quickfix/orchestra/specification/08-quickfix.md
@@ -7,17 +7,22 @@
 
 ## Messages
 
-### Message Custom Message type C1 (1)
+### Message NewOrderSingle Message type D (14)
 
 #### Synopsis
 
-An example Execution Report
+A redefined example of the NewOrderSingle message. Account has been made a required field, and an optional field RoutingID added
 
-| Sort | Name            | Tag   | Presence | Documentation                                    |
-|------|-----------------|-------|----------|--------------------------------------------------|
-| 1    | StandardHeader  | c     | required | MsgType=8                                        |
-| 2    | Account         | 1     | optional | The associated account ID.                       |
-| 3    | AvgPx           | 6     | required | Average price of fills.                          |
-| 4    | ClOrdID         | 11    | optional | 17-character Order Id sent by the source system. |
-| 5    | CumQty          | 14    | required | Total number of shares filled.                   |
-| 6    | StandardTrailer | c     | required |                                                  |
+| Sort | Name            | Tag | Presence | Documentation                                 |
+|------|-----------------|-----|----------|-----------------------------------------------|
+| 1    | StandardHeader  | c   | required | MsgType=D                                     |
+| 2    | ClOrdID         | 11  | required | The client order ID                           |
+| 3    | Account         | 1   | required | The account is required for this derived spec |
+| 4    | Side            | 54  | required | Side of the order                             |
+| 5    | TransactTime    | 60  | required | The time of this transaction                  |
+| 6    | OrdType         | 40  | required | The type of the order - buy or sell           |
+| 7    | Instrument      | c   | required | Instrument details                            |
+| 8    | OrderQtyData    | c   | required | The amount or quantity of the order           |
+| 9    | RoutingID       | 217 | optional | Optional route to a specific destination      |
+| 10   | StandardTrailer | c   | required |                                               |
+
